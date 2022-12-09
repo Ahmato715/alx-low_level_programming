4acd9e418e44caaed6042692a586e12e4b475739
Ahmato715 <Ahmeyy122@gmail.com>
2022-12-09 12:47:47 -0800

    tas 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- *return: 0
- */
+* main - main block
+* return: 0
+*/
 int main(void)
-{      
+{	
 	puts("\"programming is like develop a multilingual puzzle");
 	return (0);
 }	
