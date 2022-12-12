e63e27c2f0827b55d3dba69447d9ddda99ac7d79
Ahmato715 <Ahmeyy122@gmail.com>
2022-12-11 23:10:54 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - prints exactly and that piece of art is useful
  *
- * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
 
